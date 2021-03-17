52765f31d0f99d9b41fcf00399b84f1fbda028b8
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-17 11:11:35 -0500

    Renders category specific cases in cases comp. Need to create link to this page.

--- src/components/Cases.js
+++ src/components/Cases.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Cases = () => {
+cosnt handleCaseClick = () => {
+    console.log("click!")
+}
+
+const Cases = ({ cases }) => {
     return (
         <div>
             Here are your specific cases!
+            {cases.map (indivCase => <ul key={indivCase.id}>
+                <ol>
+                    <button onClick={handleCaseClick}>{indivCase.name}</button>
+                </ol>
+            </ul>)}
         </div>
     )
 }
 
-export default Cases;
+
+const mapStateToProps = state => {
+    return { cases: state.cases}
+}
+
+export default connect(mapStateToProps)(Cases);
