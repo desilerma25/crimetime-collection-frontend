8274b65884eb420162dd25d9723ecde936de2d19
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-18 16:45:16 -0500

    Individual case info displays.

--- src/components/Case.js
+++ src/components/Case.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const SpecificCase = () => {
+
+const SpecificCase = ({specCase}) => {
+    // debugger
     return (
         <div>
             Here is your specific case information!
+            <h1>{specCase.name}</h1>
+            <img alt={specCase.name + 'image'} src={specCase.image_url} />
+            <h4>{specCase.description}</h4>
         </div>
     )
 }
+
+
+
 
 export default SpecificCase;
